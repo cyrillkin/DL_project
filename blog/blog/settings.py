e59a9c4db81194cb596e986ea5d55ffdf6520522
cyrillkin <vision21@yandex.ru>
2021-07-11 18:24:55 +0400

    11.07

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Samara'
 
 USE_I18N = True
 
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- blog/core/views.py
+++ blog/core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse('Hello world')
+
+def test(request):
+  return HttpResponse('TEST VIEWS')
