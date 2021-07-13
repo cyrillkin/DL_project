7018c7fe63f53bc6a1914c09e8395cc293221a34
cyrillkin <vision21@yandex.ru>
2021-07-13 13:59:47 +0400

    server install

--- blog/root/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from .views import index, test
-
-urlpatterns = [
-  path('test/', test, name='test'),
-  url('', index, name='index')  
-]
