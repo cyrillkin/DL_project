f7d0ba7c6e544872d76d5a5cc30c0d4e274793f2
cyrillkin <vision21@yandex.ru>
2021-07-14 15:52:30 +0400

    Change 006

--- advito/core/urls.py
+++ advito/core/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import index, test
+from . import views
 
 urlpatterns = [
-  url('test/', test, name='test'),
-  url('', index, name='index'),
-  
+  url(r'^$', views.index, name='index'),
+  url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+  path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+  path('posts/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+  path('posts/create/', views.post_create, name='post_create'),
+  path('feed/', views.feed, name='feed'),
 ]
--- advito/core/views.py
+++ advito/core/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('Hello world')
+  return HttpResponse('1')
 
-def test(request):
-  return HttpResponse('TEST')
+def feed(request):
+  return HttpResponse('2')
+
+def post_detail(request, post_id):
+  return HttpResponse(f'{post_id}')
+
+def post_edit(request):
+  return HttpResponse('4')
+
+def post_create(request):
+  return HttpResponse('5')
+
+def post_delete(request):
+  return HttpResponse('6')
+
+def like_post(request):
+  return HttpResponse('7')
